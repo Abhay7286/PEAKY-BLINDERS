0e93acdf99b1726e13b75b575770dab698a3380d
ABHAY MISHRA <abhaymishra7286@gmail.com>
2024-11-06 12:14:59 +0530

    3rd commit
    removed hosting vercel

--- main.js
+++ main.js
@@ -1,33 +1,31 @@
 const express = require('express')
 const path = require('path')
-const connection = require("./models/conn.js")
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000  // Vercel uses dynamic port
 
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('home')
-  })
+  res.render('home')
+})
 app.get('/about', (req, res) => {
-    res.render('about')
-  })
+  res.render('about')
+})
 app.get('/news', (req, res) => {
-    res.render('news')
-  })
+  res.render('news')
+})
 app.get('/watch', (req, res) => {
-    res.render('watch')
-  })
+  res.render('watch')
+})
 app.get('/live', (req, res) => {
-    res.render('live')
-  })
+  res.render('live')
+})
 app.get('/shop', (req, res) => {
-    res.render('shop')
-  })
+  res.render('shop')
+})
 
 app.listen(port, () => {
   console.log(`Peaky Blinders listening on port ${port}`)
